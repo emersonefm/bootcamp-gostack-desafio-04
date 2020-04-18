56d4560ac3bee37115d29b3b3f862e36de6a3cc9
Emerson Maldonado <emerson.efm@gmail.com>
2020-04-18 10:32:27 -0300

    first commit

---
+++ src/components/PostList.js
@@ -0,0 +1,110 @@
+import React, { Component } from "react";
+
+import PostItem from "./PostItem";
+
+class PostList extends Component {
+  state = {
+    posts: [
+      {
+        id: 1,
+        author: {
+          name: "Emerson Maldonado",
+          avatar:
+            "https://avatars0.githubusercontent.com/u/4943303?s=460&u=7e92c2ddeace2a38fa8810abb5a2d4390e52b923&v=4",
+        },
+        date: "18 Abril 2020",
+        content: "Pessoal, alguém sabe se a Rocketseat está contratando?",
+        comments: [
+          {
+            id: 2,
+            author: {
+              name: "Diego Fernandes",
+              avatar: "https://avatars2.githubusercontent.com/u/2254731?v=4",
+            },
+            date: "18 Abril 2020",
+            content:
+              "A Rocketseat está sempre em busca de novos membros para o time, e geralmente ficamos de olho em quem se destaca no Bootcamp, inclusive 80% do nosso time de devs é composto por alunos do Bootcamp. Além disso, se você tem vontade de ensinar gravando vídeos e criando posts, pode me chamar no Discord! (Sério, me chamem mesmo, esse comentário é real)",
+          },
+        ],
+      },
+      {
+        id: 3,
+        author: {
+          name: "Neil Cook",
+          avatar: "https://i.pravatar.cc/150?img=8",
+        },
+        date: "20 Abril 2020",
+        content:
+          "Fala galera, beleza?\nEstou fazendo o Bootcamp GoStack e está sendo muito massa! Alguém mais aí fazendo? Comenta aí na publicação para trocarmos uma idéia",
+        comments: [
+          {
+            id: 4,
+            author: {
+              name: "Clara Lisboa",
+              avatar: "https://i.pravatar.cc/150?img=5",
+            },
+            date: "21 Abril 2020",
+            content:
+              "Também estou fazendo o Bootcamp e estou adorando! Estou no terceiro módulo sobre Node e já tenho minha API dos desafios construída!",
+          },
+          {
+            id: 5,
+            author: {
+              name: "Cézar Toledo",
+              avatar: "https://i.pravatar.cc/150?img=11",
+            },
+            date: "21 Abril 2020",
+            content:
+              "Que maaaaaassa! Estou pensando em me inscrever na próxima turma pra ver qual é desse Bootcamp GoStack, dizem que os devs saem de lá com super poderes",
+          },
+        ],
+      },
+      {
+        id: 6,
+        author: {
+          name: "Gabriel Lisboa",
+          avatar: "https://i.pravatar.cc/150?img=51",
+        },
+        date: "22 Abril 2020",
+        content:
+          "Fala galera, beleza?\nEstou fazendo o Bootcamp GoStack e está sendo muito massa! Alguém mais aí fazendo? Comenta aí na publicação para trocarmos uma idéia",
+        comments: [
+          {
+            id: 4,
+            author: {
+              name: "Clara Lisboa",
+              avatar: "https://i.pravatar.cc/150?img=5",
+            },
+            date: "22 Abril 2020",
+            content:
+              "Também estou fazendo o Bootcamp e estou adorando! Estou no terceiro módulo sobre Node e já tenho minha API dos desafios construída!",
+          },
+          {
+            id: 5,
+            author: {
+              name: "Cézar Toledo",
+              avatar: "https://i.pravatar.cc/150?img=11",
+            },
+            date: "23 Abril 2020",
+            content:
+              "Que maaaaaassa! Estou pensando em me inscrever na próxima turma pra ver qual é desse Bootcamp GoStack, dizem que os devs saem de lá com super poderes",
+          },
+        ],
+      },
+    ],
+  };
+
+  render() {
+    const { posts } = this.state;
+
+    return (
+      <div className="postlist">
+        {posts.map((post) => (
+          <PostItem key={post.id} {...post} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default PostList;
