00ed5b31f19bc6ae1b5919e4a41eb6dd6865256f
emersonefm <emerson.efm@gmail.com>
2020-04-18 18:26:13 -0300

    ajuste postlist

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -21,7 +21,7 @@ class PostList extends Component {
               name: "Diego Fernandes",
               avatar: "https://avatars2.githubusercontent.com/u/2254731?v=4",
             },
-            date: "18 Abril 2020",
+            date: "19 Abril 2020",
             content:
               "A Rocketseat está sempre em busca de novos membros para o time, e geralmente ficamos de olho em quem se destaca no Bootcamp, inclusive 80% do nosso time de devs é composto por alunos do Bootcamp. Além disso, se você tem vontade de ensinar gravando vídeos e criando posts, pode me chamar no Discord! (Sério, me chamem mesmo, esse comentário é real)",
           },
@@ -43,7 +43,7 @@ class PostList extends Component {
               name: "Clara Lisboa",
               avatar: "https://i.pravatar.cc/150?img=5",
             },
-            date: "21 Abril 2020",
+            date: "20 Abril 2020",
             content:
               "Também estou fazendo o Bootcamp e estou adorando! Estou no terceiro módulo sobre Node e já tenho minha API dos desafios construída!",
           },
